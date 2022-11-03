653b6a7febf3f163b3a758e79754e0d9b7dc1da5
BaeInHoo <qodlsgh0210@naver.com>
2022-11-04 00:14:41 +0900

    JPA를 통한 CRUD 배우기

--- src/main/java/com/example/springadmin/repository/UserRepository.java
+++ src/main/java/com/example/springadmin/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.example.springadmin.repository;
 
 import com.example.springadmin.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
--- src/test/java/com/example/springadmin/repository/UserRepositoryTest.java
+++ src/test/java/com/example/springadmin/repository/UserRepositoryTest.java
@@ -2,16 +2,19 @@ package com.example.springadmin.repository;
 
 import com.example.springadmin.SpringAdminApplicationTests;
 import com.example.springadmin.model.entity.User;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import com.example.springadmin.repository.UserRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 public class UserRepositoryTest extends SpringAdminApplicationTests {
 
     @Autowired
     private UserRepository userRepository;
 
+    @Test
     public void create() {
         User user = new User();
         user.setAccount("TestUser01");
@@ -20,16 +23,36 @@ public class UserRepositoryTest extends SpringAdminApplicationTests {
         user.setCreatedAt(LocalDateTime.now());
         user.setCreatedBy("TestUser01 admin");
 
+        User newUser = userRepository.save(user);
+        System.out.println("new User: " + newUser);
+
     }
 
-    public void read() {
+    @Test
+    public void read(@RequestParam Long id) {
+        Optional<User> user = userRepository.findById(2L);
+
+        user.ifPresent(selectUser -> {
+            System.out.println("user : " + selectUser);
+            System.out.println("email : " + selectUser.getEmail());
+        });
 
     }
 
+    @Test
     public void update() {
+        Optional<User> user = userRepository.findById(2L);
 
+        user.ifPresent(selectUser -> {
+            selectUser.setAccount("PPP");
+            selectUser.setUpdatedAt(LocalDateTime.now());
+            selectUser.setUpdateBy("update method()");
+
+            userRepository.save(selectUser);
+        });
     }
 
+    @Test
     public void delete() {
 
     }
