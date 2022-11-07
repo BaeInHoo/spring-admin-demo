66b50aada9d87b0ced3a30edd92453f7df5d11cd
BaeInHoo <qodlsgh0210@naver.com>
2022-11-07 15:06:03 +0900

    partner item order detail TABLE Test

--- src/test/java/com/example/springadmin/repository/OrderDetailRepositoryTest.java
+++ src/test/java/com/example/springadmin/repository/OrderDetailRepositoryTest.java
@@ -2,10 +2,13 @@ package com.example.springadmin.repository;
 
 import com.example.springadmin.SpringAdminApplicationTests;
 import com.example.springadmin.model.entity.OrderDetail;
+import org.aspectj.weaver.ast.Or;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,6 +20,20 @@ class OrderDetailRepositoryTest extends SpringAdminApplicationTests {
 
     @Test
     public void create() {
+
+        OrderDetail orderDetail = new OrderDetail();
+
+        orderDetail.setStatus("WAITING");
+        orderDetail.setArrivalDate(LocalDateTime.now().plusDays(2));
+        orderDetail.setQuantity(1);
+        orderDetail.setTotalPrice(BigDecimal.valueOf(900000));
+        orderDetail.setOrderGroupId(7L);
+        orderDetail.setItemId(1L);
+        orderDetail.setCreatedAt(LocalDateTime.now());
+        orderDetail.setCreatedBy("AdminServer");
+
+        OrderDetail newOrderDetail = orderDetailRepository.save(orderDetail);
+        Assertions.assertNotNull(newOrderDetail);
 
     }
 
