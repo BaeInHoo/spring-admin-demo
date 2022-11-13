a67bc9cd6fe7024906d682d21fb34936b4b4b400
BaeInHoo <qodlsgh0210@naver.com>
2022-11-13 12:03:15 +0900

    Controller Refactoring

--- src/main/java/com/example/springadmin/controller/api/OrderGroupApiController.java
+++ src/main/java/com/example/springadmin/controller/api/OrderGroupApiController.java
@@ -1,5 +1,6 @@
 package com.example.springadmin.controller.api;
 
+import com.example.springadmin.controller.CrudController;
 import com.example.springadmin.controller.ifs.CrudInterface;
 import com.example.springadmin.model.network.Header;
 import com.example.springadmin.model.network.request.OrderGroupApiRequest;
@@ -10,34 +11,18 @@ import com.example.springadmin.service.OrderGroupApiLogicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
+
 @RestController
 @RequestMapping("/api/orderGroup")
-public class OrderGroupApiController implements CrudInterface<OrderGroupApiRequest, OrderGroupApiResponse> {
+public class OrderGroupApiController extends CrudController<OrderGroupApiRequest, OrderGroupApiResponse> {
 
     @Autowired
     private OrderGroupApiLogicService orderGroupApiLogicService;
 
-    @Override
-    @PostMapping("")
-    public Header<OrderGroupApiResponse> create(@RequestBody Header<OrderGroupApiRequest> request) {
-        return orderGroupApiLogicService.create(request);
+    @PostConstruct
+    public void init() {
+        this.baseService = orderGroupApiLogicService;
     }
 
-    @Override
-    @GetMapping("{id}")
-    public Header<OrderGroupApiResponse> read(@PathVariable Long id) {
-        return orderGroupApiLogicService.read(id);
-    }
-
-    @Override
-    @PutMapping("")
-    public Header<OrderGroupApiResponse> update(@RequestBody Header<OrderGroupApiRequest> request) {
-        return orderGroupApiLogicService.update(request);
-    }
-
-    @Override
-    @DeleteMapping("{id}")
-    public Header delete(@PathVariable Long id) {
-        return orderGroupApiLogicService.delete(id);
-    }
 }
